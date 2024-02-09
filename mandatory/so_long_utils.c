56a405221b1540a9af5a0c68113237abb4965b1c
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-02-09 16:54:42 +0100

    almost done all norminette

--- mandatory/so_long_utils.c
+++ mandatory/so_long_utils.c
@@ -6,17 +6,20 @@
 /*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 21:38:47 by houamrha          #+#    #+#             */
-/*   Updated: 2024/02/08 21:40:04 by houamrha         ###   ########.fr       */
+/*   Updated: 2024/02/09 16:54:08 by houamrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int last_line(int fd)
+int	last_line(int fd)
 {
-	int i = 0;
-	char *s = get_next_line(fd);
-	while(s)
+	int		i;
+	char	*s;
+
+	i = 0;
+	s = get_next_line(fd);
+	while (s)
 	{
 		i++;
 		free(s);
@@ -26,25 +29,30 @@ int last_line(int fd)
 	return (i);
 }
 
-char **map_to_array(int fd, int last)
+char	**map_to_array(int fd, int last)
 {
-	int i = 0;
-	char **map_array = (char **)malloc((last + 1) * sizeof(char *));
+	int		i;
+	char	**map_array;
+
+	i = 0;
+	map_array = (char **)malloc((last + 1) * sizeof(char *));
 	if (!map_array)
 		return (NULL);
-	while(i < last)
+	while (i < last)
 	{
 		map_array[i] = get_next_line(fd);
 		i++;
 	}
 	map_array[i] = NULL;
-	return(map_array);
+	return (map_array);
 }
 
-void free_array(char **array)
+void	free_array(char **array)
 {
-	int i = 0;
-	while(array[i])
+	int	i;
+
+	i = 0;
+	while (array[i])
 	{
 		free(array[i]);
 		i++;
@@ -52,13 +60,13 @@ void free_array(char **array)
 	free(array);
 }
 
-void freeing(mlx_data data)
+void	freeing(mlx_data data)
 {
 	free_array(data.map_array);
 	free_array(data.map_array_copy);
 }
 
-void print_moves_count(mlx_data *data)
+void	print_moves_count(mlx_data *data)
 {
 	data->moves += 1;
 	ft_putnbr_fd(data->moves, 1);
