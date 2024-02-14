0af0ec8cba7aa5eb6dc0d07822732f9e42401f2d
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-02-14 16:11:51 +0100

    all

--- bonus/animate_player.c
+++ bonus/animate_player.c
@@ -6,7 +6,7 @@
 /*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 16:13:22 by houamrha          #+#    #+#             */
-/*   Updated: 2024/02/14 15:42:14 by houamrha         ###   ########.fr       */
+/*   Updated: 2024/02/14 16:11:38 by houamrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	animate_player_r(t_mlx_data *data)
 {
+	if (data->map_array[data->yp][data->xp] != 'E')
+		mlx_put_image_to_window(data->mlx, data->window, data->back_img,
+			data->xp * 48, data->yp * 48);
 	if (data->flag == 0)
 		mlx_put_image_to_window(data->mlx, data->window, data->player_img,
 			data->xp * 48, data->yp * 48);
@@ -30,6 +33,9 @@ void	animate_player_r(t_mlx_data *data)
 
 void	animate_player_l(t_mlx_data *data)
 {
+	if (data->map_array[data->yp][data->xp] != 'E')
+		mlx_put_image_to_window(data->mlx, data->window, data->back_img,
+			data->xp * 48, data->yp * 48);
 	if (data->flag == 0)
 		mlx_put_image_to_window(data->mlx, data->window, data->player_l_img,
 			data->xp * 48, data->yp * 48);
@@ -56,9 +62,6 @@ int	animate_player(t_mlx_data *data)
 		put_enemy(data);
 	if (data->timing == 1000)
 	{
-		if (data->map_array[data->yp][data->xp] != 'E')
-			mlx_put_image_to_window(data->mlx, data->window, data->back_img,
-				data->xp * 48, data->yp * 48);
 		if (data->dir == 0)
 			animate_player_r(data);
 		else
